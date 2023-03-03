d1449cbf2ee09a3b839857147c5324ebf88e15c8
Patrik Fejda <patrikfejda@gmail.com>
2023-03-03 13:34:31 +0100

    use sqlalchemy

--- app/database.py
+++ app/database.py
@@ -1,11 +1,39 @@
 import psycopg2
 from app.config import settings
+from sqlalchemy import create_engine
 
+host = settings.DATABASE_HOST
+port = settings.DATABASE_PORT
+user = settings.DATABASE_USER
+password = settings.DATABASE_PASSWORD
+database = settings.DATABASE_NAME
 
 cur = psycopg2.connect(
-    host=settings.DATABASE_HOST,
-    port=settings.DATABASE_PORT,
-    user=settings.DATABASE_USER,
-    password=settings.DATABASE_PASSWORD,
-    database=settings.DATABASE_NAME,
+    host=host,
+    port=port,
+    user=user,
+    password=password,
+    database=database,
 ).cursor()
+
+conn = create_engine(
+    f"postgresql://{user}:{password}@{host}:{port}/{database}", future=True
+).connect()
+
+
+# def connect(db_uri):
+#     engine = create_engine(db_uri)
+#     return engine.connect()
+
+
+# database = settings.DATABASE_NAME
+
+# try:
+#     db_uri = os.path.expandvars(
+#         f"postgresql://{user}:{password}@{host}:{port}/{database}"
+#     )
+#     db_uri = urllib.parse.unquote(db_uri)
+#     conn = connect(db_uri)
+# except Exception as e:
+#     print("Failed to connect to database.")
+#     print("{0}".format(e))
--- app/endpoints/v1/dbhealth.py
+++ app/endpoints/v1/dbhealth.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
+from sqlalchemy import text
 
-from app.database import cur
+from app.database import cur, conn
  
 router = APIRouter()
 
 @router.get("/v1/dbhealth")
 async def healthcheck():
-    cur.execute("SELECT version();")
-    version = cur.fetchone()[0]
+    # cur.execute("SELECT version();")
+    # version = cur.fetchone()[0]
+    result = conn.execute(text("SELECT version();"))
+    version = (result.one()[0])
     return {"version": version}
