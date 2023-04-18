260b2de336885e5870590ad7ea498a19e4c6b3e2
patrikfejda <patrikfejda@gmail.com>
2023-04-18 09:28:56 +0200

    Display name is set to username automatically

--- app/crud/user.py
+++ app/crud/user.py
@@ -23,7 +23,7 @@ def get_user_by_username(db: Session, username: str):
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
-        email=user.email, username=user.username, hashed_password=get_password_hash(user.password)
+        email=user.email, username=user.username, hashed_password=get_password_hash(user.password), display_name=user.username
     )
     db.add(db_user)
     db.commit()
