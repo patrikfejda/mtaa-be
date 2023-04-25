b68f07d3c8413eef8457d70a1677a4dacc2cd303
patrikfejda <patrikfejda@gmail.com>
2023-04-25 11:48:13 +0200

    Fix

--- app/api/endpoints/v2/statuses.py
+++ app/api/endpoints/v2/statuses.py
@@ -13,9 +13,9 @@ def get_all_statuses(current_user: CurrentUserDependency, db: SessionDependency)
     return crud.get_all_statuses(db=db)
 
 
-@router.post("/", response_model=schemas.Status, current_user=CurrentUserDependency, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=schemas.Status, status_code=status.HTTP_201_CREATED)
 def create_status(
-    status_create: schemas.StatusCreate, db: SessionDependency
+    status_create: schemas.StatusCreate, current_user: CurrentUserDependency, db: SessionDependency
 ):
     # CHECK FOR NOT APPROPRIATE WORDS
     normalized_sentence = unicodedata.normalize('NFKD', status_create.text).encode('ASCII', 'ignore').decode('utf-8')
